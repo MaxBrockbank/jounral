e8e47b64b1f9bde009a284108b1d6314ea586133
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-09 15:41:29 -0800

    add jquery, bootstrap and eslint plugins/configs
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require ('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require ('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -7,11 +8,32 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
       inject: 'body'
     })
-  ]
+  ],
+  modules: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  }
 }
